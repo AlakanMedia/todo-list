bcbb5a19afe7f589332667c930d9ec233fd49633
AlakanMedia <alakan439@gmail.com>
2024-02-24 22:36:38 -0500

    I modified the main and Makefile

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
 all: program
 
-program: main.o list.o user_interaction.o
-	gcc -o todo_list main.o list.o user_interaction.o
+program: main.o list.o user_interaction.o misc_funtions.o
+	gcc -o todo_list main.o list.o user_interaction.o misc_funtions.o
 
-main.o: main.c list.h user_interaction.h
+main.o: main.c list.h user_interaction.h misc_funtions.h
 	gcc -c main.c
 
-list.o: list.c list.h todo_element.h
+list.o: list.c list.h todo_element.h misc_funtions.h
 	gcc -c list.c
 
 user_interaction.o: user_interaction.c user_interaction.h
 	gcc -c user_interaction.c
+
+misc_funtions.o: misc_funtions.c misc_funtions.h
+	gcc -c misc_funtions.c
 
 clean:
 	rm -f *.o todo_list
--- main.c
+++ main.c
@@ -1,5 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+#include "user_interaction.h"
+#include "misc_funtions.h"
+
 int
 main()
 {
+    list *the_list = (list *) malloc(sizeof(list));
+
+    if (the_list == NULL)
+	return 1;
+
+    the_list->head = NULL;
+    the_list->tail = NULL;
+    the_list->num_elements = 0;
+
+    int option = 0;
+
+    while (option != 5)
+    {
+	system("clear");
+	draw_menu_options();
+	printf("\nChoose an option: ");
+	scanf("%d", &option);
+	clean_buffer();
+
+	switch (option)
+	{
+	    case 1:
+		system("clear");
+		create_todo(the_list);
+		printf("Press any key to exit... ");
+		getchar();
+		clean_buffer();
+		break;
+	    case 2:
+		system("clear");
+		view_todos(the_list);
+		printf("Press any key to exit... ");
+		getchar();
+		clean_buffer();
+		break;
+	    case 3:
+		break;
+	    case 4:
+		break;
+	}
+    }
+
+    free(the_list);
+    the_list = NULL;
+
     return 0;
 }
+
+
